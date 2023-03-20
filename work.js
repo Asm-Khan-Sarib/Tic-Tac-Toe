3d7ecc274eaa9d3bdde28f72d57e386b0bc72420
Asm <asm.khan.sarib98@gmail.com>
2023-03-20 19:37:50 +0600

    added background image and music

---
+++ work.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // backgroud image change
+    document.getElementById("t1").addEventListener("click", function(){
+        document.body.style.backgroundImage = 'url("image/space1.jpg")';
+    })
+    document.getElementById("t2").addEventListener("click", function(){
+        document.body.style.backgroundImage = 'url("image/underwater1.jpg")';
+    })
+    document.getElementById("t3").addEventListener("click", function(){
+        document.body.style.backgroundImage = 'url("image/nature2.jpg")';
+    })
+    // backgroud music
+    document.getElementById("m1").addEventListener("click", function(){
+        document.getElementById("background-audio").play();
+    })
+    document.getElementById("m2").addEventListener("click", function(){
+        document.getElementById("background-audio").pause();
+    })
+})
